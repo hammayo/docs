aa779f585efa8b58c0aff44bb97953140204896b
Hammy Babar <hammy@hammayo.co.uk>
2025-04-14 00:38:09 +0100

    Initial version

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    './**/*.mdx',
+    './**/*.tsx',
+    './**/*.jsx',
+    './**/*.js',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#16A34A',
+          dark: '#15803D',
+        },
+      },
+    },
+  },
+  plugins: [],
+} 
